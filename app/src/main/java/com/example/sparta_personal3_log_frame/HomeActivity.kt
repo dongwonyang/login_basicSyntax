89a66dddc9c24affe28490632ccd58fdf0956f88
yang dong won <didehddnjs89@gmail.com>
2024-03-18 19:47:01 +0900

    lv3

--- app/src/main/java/com/example/sparta_personal3_log_frame/HomeActivity.kt
+++ app/src/main/java/com/example/sparta_personal3_log_frame/HomeActivity.kt
@@ -2,10 +2,22 @@ package com.example.sparta_personal3_log_frame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+
+        val buttonFinish = findViewById<Button>(R.id.button_home_finish)
+        val textViewId = findViewById<TextView>(R.id.textView_home_id)
+
+        textViewId.text = intent.getStringExtra("Email")
+
+        buttonFinish.setOnClickListener {
+            finish()
+        }
     }
 }
